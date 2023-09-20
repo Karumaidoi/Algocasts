55875bd36588a9aae99b9147000c1e7d7da0718a
Karumaidoi <mainahmwangi12@gmail.com>
2023-09-21 06:58:51 +0700

    Recap - maxChars

--- maxChars.js
+++ maxChars.js
@@ -1,19 +1,19 @@
 function maxChars(str) {
-  let chars = {};
+  let charsMap = {};
   let max = 0;
   let maxChar = "";
 
-  for (let char of str) {
-    if (!chars[char]) {
-      chars[char] = 1;
+  for (const char of str) {
+    if (!charsMap[char]) {
+      charsMap[char] = 1;
     } else {
-      chars[char]++;
+      charsMap[char]++;
     }
   }
 
-  for (const char in chars) {
-    if (chars[char] > max) {
-      max = chars[char];
+  for (let char in charsMap) {
+    if (charsMap[char] > max) {
+      max = charsMap[char];
       maxChar = char;
     }
   }
